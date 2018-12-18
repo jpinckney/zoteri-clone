2335b12620eb7f02f8e2a5d4d74e0c133156ad22
Jeff Pinckney <jeffpinckney.90@gmail.com>
2018-12-17 20:18:55 -0700

    first commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        Shop About How To Measure Login
+      </div>
+    )
+  }
+}
